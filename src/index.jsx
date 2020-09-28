69ab1149d96becb2b701574217e6567a6817ca8f
kimuniiii <kk.baseball.0824@gmail.com>
2020-09-28 17:01:45 +0900

    BootStrapを用いたイベントフォームの作成

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MultipleState from './components/Multiple-state';
+import App from './components/App';
 
-ReactDOM.render(<MultipleState />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
